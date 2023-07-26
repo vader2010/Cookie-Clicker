65a2ad1705bfeffdafcdfd85fad2ead968a04bed
Michael Cutsail <65092568+vader2010@users.noreply.github.com>
2023-07-25 19:59:32 -0700

    Update cookie_game.py

--- cookie_game.py
+++ cookie_game.py
@@ -4,9 +4,9 @@ from pygame import *
 pygame.init()
 X = 1920
 Y = 1080
-app = pygame.display.set_mode((X, Y), pygame.FULLSCREEN)
-cookie = pygame.image.load("C:\\Users\\Student\\Desktop\\Michael C\\Cookie Clicker\\cookie.png").convert_alpha()
-kavoon_font = pygame.font.Font("Kavoon-Regular.ttf", 18)
+app = pygame.display.set_mode((X, Y))
+cookie = pygame.image.load("cookie.png").convert_alpha()
+kavoon_font = pygame.font.Font("Kavoon-Regular.ttf", 32)
 cookie_scaled = pygame.transform.scale(cookie, (350, 350))
 app.blit(cookie_scaled, (50, 270))
 cookie_mask = pygame.mask.from_surface(cookie_scaled)
@@ -15,8 +15,11 @@ clicker.fill((255, 0, 0))
 clicker_mask = pygame.mask.from_surface(clicker)
 pygame.display.flip()
 running = True
-cookies = 0
+cookies1 = 0
+cookie_clicked = 0
+clicking = False
 while running:
+    cookie_clicked += 1
     for event in pygame.event.get():
         app.fill((0, 0, 0))
         coordinates = pygame.mouse.get_pos()
@@ -25,14 +28,22 @@ while running:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 running = False
-        if pygame.mouse.get_pressed()[0]:
+        if pygame.mouse.get_pressed()[0] and not clicking:
+            clicking = True
             print('clicked')
-            if clicker_mask.overlap(cookie_mask, (coordinates[0] - cookie_scaled.get_rect().x, coordinates[1] - cookie_scaled.get_rect().y)):
-                print("run")
-                cookies += 1
-                print(cookies)
-        app.blit(cookie_scaled,  (50, 270))
+            offset_x = coordinates[0] - 50
+            offset_y = coordinates[1] - 270
+            if cookie_mask.overlap(clicker_mask, (offset_x, offset_y)):
+                if cookie_clicked > 5:
+                    cookies1 += 1
+                    cookie_clicked = 0
+        elif not pygame.mouse.get_pressed()[0] and clicking:
+            clicking = False
+        app.blit(cookie_scaled, (50, 270))
         app.blit(clicker, coordinates)
+        cookies2 = kavoon_font.render(f"Cookies: {cookies1}", True, (255, 255, 255))
+        app.blit(cookies2, (0, 0))
         pygame.display.flip()
+
 
 pygame.quit()
